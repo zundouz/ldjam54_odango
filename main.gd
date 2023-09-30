a3e8f03a385b3b2205e965f93a43441d3e72e87a
zundou <h240503@gmail.com>
2023-09-30 15:22:20 +0900

    feat: Signal 成功していた
    - start も
    - mobtimer も

--- main.gd
+++ main.gd
@@ -11,30 +11,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-		# Create a new instance of the Mob scene.
-	var mob = mob_scene.instantiate()
-
-	# Choose a random location on Path2D.
-	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
-	print(mob_spawn_location)
-	mob_spawn_location.progress_ratio = randf()
-
-	# Set the mob's direction perpendicular to the path direction.
-	var direction = mob_spawn_location.rotation + PI / 2
-
-	# Set the mob's position to a random location.
-	mob.position = mob_spawn_location.position
-
-	# Add some randomness to the direction.
-	direction += randf_range(-PI / 4, PI / 4)
-	mob.rotation = direction
-
-	# Choose the velocity for the mob.
-	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
-	mob.linear_velocity = velocity.rotated(direction)
-
-	# Spawn the mob by adding it to the Main scene.
-	add_child(mob)
+	pass
 
 func game_over():
 	# $ScoreTimer.stop()
@@ -51,30 +28,31 @@ func _on_StartTimer_timeout():
 	# $ScoreTimer.start()
 
 func _on_mob_timer_timeout():
-	# Create a new instance of the Mob scene.
-	var mob = mob_scene.instantiate()
-
 	# Choose a random location on Path2D.
 	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
-	print(mob_spawn_location)
 	mob_spawn_location.progress_ratio = randf()
 
 	# Set the mob's direction perpendicular to the path direction.
 	var direction = mob_spawn_location.rotation + PI / 2
+	print("mob timer timeout よばれた")
+	
+	if true:
+		# Create a new instance of the Mob scene.
+		var mob = mob_scene.instantiate()
+		# Set the mob's position to a random location.
+		mob.position = mob_spawn_location.position
 
-	# Set the mob's position to a random location.
-	mob.position = mob_spawn_location.position
+		# Add some randomness to the direction.
+		direction += randf_range(-PI / 4, PI / 4)
+		mob.rotation = direction
 
-	# Add some randomness to the direction.
-	direction += randf_range(-PI / 4, PI / 4)
-	mob.rotation = direction
+		# Choose the velocity for the mob.
+		var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+		mob.linear_velocity = velocity.rotated(direction)
 
-	# Choose the velocity for the mob.
-	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
-	mob.linear_velocity = velocity.rotated(direction)
+		# Spawn the mob by adding it to the Main scene.
+		add_child(mob)
 
-	# Spawn the mob by adding it to the Main scene.
-	add_child(mob)
 
 #func _on_start_timer_timeout():
 #	#func _on_StartTimer_timeout():
