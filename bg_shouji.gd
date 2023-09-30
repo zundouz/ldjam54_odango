9bb63a3ffdddfec6d4a8949c0a45a61f8dce71df
zundou <h240503@gmail.com>
2023-09-30 17:59:53 +0900

    style: デフォルト背景色に色をつけてみる

--- bg_shouji.gd
+++ bg_shouji.gd
@@ -6,7 +6,6 @@ func _ready() -> void:
 	$AnimatedSprite2D.play()
 	z_index = 100
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
