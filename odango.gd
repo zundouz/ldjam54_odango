989fdadbc1d4454c9eec8c80ba9efeb15208867e
zundou <h240503@gmail.com>
2023-09-30 19:05:18 +0900

    feat: 多分スロー演出は上手くいってる

--- odango.gd
+++ odango.gd
@@ -4,6 +4,8 @@ var Center_X_Pos : float = 320
 var is_shot : bool = false
 var death_hide_counter : int = 0
 
+var is_already_slowed : bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():	
 	# TODO: 乱数で色変更
@@ -38,12 +40,29 @@ func _process(delta):
 			# その後、お団子が消える
 			queue_free()
 			
-	if transform.get_origin().x < Center_X_Pos + 32 and transform.get_origin().x > Center_X_Pos - 32:
-		pass
-	else:
+	if is_shot == false:		
 		if MyGlobal.is_odango_finished == false:
-			# TODO: 当たってない団子をちょっと地味にする演出欲しい	
-			pass
+			# TODO: 当たってない団子をちょっと地味にする演出欲しい
+			is_already_slowed = true
+			if linear_velocity.x >= 0:
+				linear_velocity = Vector2(50.0, 0.0)
+			else:
+				linear_velocity = Vector2(-50.0, 0.0)
+
+		else:
+			if transform.get_origin().x < Center_X_Pos + 40 and transform.get_origin().x > Center_X_Pos - 40:
+				# すでにスローになっている団子が真ん中で詰まられると困る
+				if is_already_slowed:
+					if linear_velocity.x >= 0:
+						linear_velocity = Vector2(randf_range(150.0, 300.0), 0.0)
+					else:
+						linear_velocity = Vector2(randf_range(-300.0, -150.0), 0.0)
+			else:
+				if linear_velocity.x >= 0:
+					linear_velocity = Vector2(randf_range(150.0, 300.0), 0.0)
+				else:
+					linear_velocity = Vector2(randf_range(-300.0, -150.0), 0.0)
+
 
 ## 画面外に言ったらfreeして消えてもらう
 #func _on_visible_on_screen_notifier_2d_screen_exited():
