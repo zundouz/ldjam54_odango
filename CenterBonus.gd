dcfe95106c5c4c46cbde881d3842477276f4d37f
zundou <h240503@gmail.com>
2023-10-01 19:08:00 +0900

    feat: z_index で得点UIが一番手前に来るように

--- CenterBonus.gd
+++ CenterBonus.gd
@@ -3,6 +3,7 @@ var is_already_blinked : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	z_index = 101 # 障子より前
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
