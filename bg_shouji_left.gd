afc437ab3a57a3993a83ef8fbd77576f03283fac
zundou <h240503@gmail.com>
2023-10-02 05:49:08 +0900

    feat: result で扉閉める

--- bg_shouji_left.gd
+++ bg_shouji_left.gd
@@ -1,10 +1,15 @@
 extends RigidBody2D
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimatedSprite2D.play()
+	pass # Replace with function body.
 	z_index = 100
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if MyGlobal.game_state == MyGlobal.game_state_type.Result:
+		position.x = 480
+	else:
+		position.x = 512
